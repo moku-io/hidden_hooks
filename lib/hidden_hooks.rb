83f69174638080a3a60c38fc44b30ef486611f98
Riccardo Agatea <riccardo@moku.io>
2025-01-24 12:12:56 +0100

    implement gem

--- lib/hidden_hooks.rb
+++ lib/hidden_hooks.rb
@@ -1,1 +1,46 @@
 require_relative 'hidden_hooks/version'
+
+require 'active_support/core_ext/module/attribute_accessors'
+
+module HiddenHooks
+  mattr_reader :hooks,
+               default: Hash.new { |h1, k1| h1[k1] = Hash.new { |h2, k2| h2[k2] = [] } },
+               instance_accessor: false,
+               instance_reader: false
+
+  class SetUpProxy
+    private
+
+    def method_missing hook, klass, &block
+      ::HiddenHooks.hooks[klass][hook] << block
+    end
+
+    def respond_to_missing? _, _=false
+      true
+    end
+  end
+
+  class LookUpProxy
+    def initialize klass
+      @hooks = ::HiddenHooks.hooks[klass]
+    end
+
+    private
+
+    def method_missing(hook, *args, **kwargs, &block)
+      @hooks[hook].each { _1.call(*args, **kwargs, &block) }
+    end
+
+    def respond_to_missing? _, _=false
+      true
+    end
+  end
+
+  def self.hook_up(&block)
+    SetUpProxy.new.instance_exec(&block)
+  end
+
+  def self.[] klass
+    LookUpProxy.new klass
+  end
+end
