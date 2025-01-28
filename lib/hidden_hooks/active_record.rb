c34f219c2b97e49e978c14e315399faec62ca808
Riccardo Agatea <riccardo@moku.io>
2025-01-28 16:45:03 +0100

    make active record integration actually work

--- lib/hidden_hooks/active_record.rb
+++ lib/hidden_hooks/active_record.rb
@@ -4,30 +4,34 @@ module HiddenHooks
   module ActiveRecord
     extend ActiveSupport::Concern
 
-    included do
-      [
-        :after_commit,
-        :after_create,
-        :after_create_commit,
-        :after_destroy,
-        :after_destroy_commit,
-        :after_find,
-        :after_initialize,
-        :after_rollback,
-        :after_save,
-        :after_save_commit,
-        :after_touch,
-        :after_update,
-        :after_update_commit,
-        :after_validation,
-        :before_commit,
-        :before_create,
-        :before_destroy,
-        :before_save,
-        :before_update,
-        :before_validation
-      ].each do |callback|
-        send callback, HiddenHooks[self], prepend: true
+    class_methods do
+      def inherited subclass
+        super
+
+        [
+          :after_commit,
+          :after_create,
+          :after_create_commit,
+          :after_destroy,
+          :after_destroy_commit,
+          :after_find,
+          :after_initialize,
+          :after_rollback,
+          :after_save,
+          :after_save_commit,
+          :after_touch,
+          :after_update,
+          :after_update_commit,
+          :after_validation,
+          :before_commit,
+          :before_create,
+          :before_destroy,
+          :before_save,
+          :before_update,
+          :before_validation
+        ].each do |callback|
+          subclass.send callback, HiddenHooks[subclass], prepend: true
+        end
       end
     end
   end
